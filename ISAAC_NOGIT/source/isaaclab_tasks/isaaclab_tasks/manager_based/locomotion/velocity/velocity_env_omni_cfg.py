5c432eb08c28c88512e5590c3c20ad62858c2299
ducciopet <ducciopetreni00@gmail.com>
2025-09-12 10:54:42 +0200

    Restored cfg

--- ISAAC_NOGIT/source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/velocity_env_omni_cfg.py
+++ ISAAC_NOGIT/source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/velocity_env_omni_cfg.py
@@ -105,7 +105,7 @@ class ActionsCfg:
     """Action specifications for the MDP."""
 
     joint_pos = mdp.JointPositionActionCfg(asset_name="robot", joint_names= [".*HFE", ".*KFE"], scale=0.5, use_default_offset=True)
-    joint_vel = mdp.JointVelocityActionCfg(asset_name="robot", joint_names=[".*ANKLE"], scale=2.0, use_default_offset=True)
+    joint_vel = mdp.JointVelocityActionCfg(asset_name="robot", joint_names=[ ".*ANKLE"], scale=2.0, use_default_offset=True)
 
 
 @configclass
